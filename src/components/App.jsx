d5d3b47f910a01ef24a0a1d333a83c2372757c6f
Aleksei Yarantsev <aleksei.yarantsev@gmail.com>
2021-03-26 20:30:44 -0700

    add notes.js with the notes array, render all notes to Note component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,30 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes"
+
+
+function createNote(note, index){
+console.log("this is note", note)
+return (
+  <Note 
+  key={index}
+  id={index}
+  title={note.title}
+  noteBody={note.content}
+  />
+);
+}
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {/* <Note 
+      title={notes[0].title}
+      noteBody={notes[0].content}
+      /> */}
+      {notes.map(createNote)}
       <Footer />
     </div>
   );
